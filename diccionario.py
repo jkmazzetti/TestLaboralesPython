7bfaf13bfce87b8bc8c544da1ceec9a103f9162e
Jeremaz <jere.mazzetti@gmail.com>
2022-05-10 18:23:01 -0300

    Ejercicio de conversion de lista a diccionario contabilizando veces que se repiten elementos.

---
+++ diccionario.py
@@ -0,0 +1,19 @@
+"""Dada la lista=[1,1,1,2,5,4,8,8,7,5,0,-1] contabilizar cuantas veces se repite cada
+elemento."""
+
+def contador_de_elementos(una_lista):
+    mi_dict=dict()
+    for elemento in una_lista:
+        if elemento not in mi_dict:
+            mi_dict.update({elemento:1})
+        else:
+            mi_dict.update({elemento:mi_dict.get(elemento)+1})
+    return mi_dict
+
+
+
+
+
+
+lista=[1,1,1,2,5,4,8,8,7,5,0,-1]
+print(contador_de_elementos(lista))
