f2b201f08ad27595d0e1008f599c67d78a33beb6
Jeremaz <jere.mazzetti@gmail.com>
2022-05-11 02:12:34 -0300

    Se agrega ejercicio de analisis y solucion de errores.

--- solucionar_errores_1.py
+++ solucionar_errores_1.py
@@ -4,11 +4,9 @@ modo que puede ser ejecutado y explicar que es lo que hace:
 lista_a=[1,1,1,2,5,4,8,8,7,5,0,-1]
 lista_b=[7,5,0,-1]
 lista_c=list()
-
 for i in range(lista_a):
     t=lista_a[i]+lista_b[i]
     lista_c.append(t)
-
 print (lista_c)
 
 En este ejercicio hay dos errores:
