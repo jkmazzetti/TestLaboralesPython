e7ab76ab830364edd3521d56fc337a57ba347bd3
Jeremaz <jere.mazzetti@gmail.com>
2022-05-11 02:18:41 -0300

    Se agrega ejercicio de busqueda binaria no recursiva.

--- busqueda_binaria_no_recursiva.py
+++ busqueda_binaria_no_recursiva.py
@@ -1,15 +1,15 @@
-"""Dada la siguiente lista ordenada, realizar la busqueda bineria del elemento 77.
-lista=[1,2,3,4,5,6,7,8,9,10] devolver indice"""
+"""Dada la siguiente lista ordenada, lista=[1,2,3,4,5,6,7,8,9,10], realizar las busqueda binarias de:
 
-
-"""
-elemento=1 devuelve 0
-elemento=10 devuelve 9
-elemento=7 devuelve 6
 elemento=0 devuelve -1
+elemento=1 devuelve 0
+elemento=7 devuelve 6
+elemento=10 devuelve 9
+
 inicio=0
 fin=len(lista)
 medio=(fin+inicio)/2.__round()
+
+Busqueda binaria no recursiva.
 """
 
 def busqueda_binaria(lista,elemento):
@@ -31,9 +31,9 @@ def busqueda_binaria(lista,elemento):
     return posicion
 
 lista=[1,2,3,4,5,6,7,8,9,10]
-print(1,":",busqueda_binaria(lista,1))
-print(10,":",busqueda_binaria(lista,10))
-print(7,":",busqueda_binaria(lista,7))
 print(0,":",busqueda_binaria(lista,0))
+print(1,":",busqueda_binaria(lista,1))
+print(7,":",busqueda_binaria(lista,7))
+print(10,":",busqueda_binaria(lista,10))
 
 
